0d5801cef1967050d97bc78047bbee444d0ca33d
Julien Cassette <julien.cassette@gmail.com>
2015-11-20 22:16:50 +0100

    Use non blocking mode for the drone

--- embedded-sw/serial-example/serial.c
+++ embedded-sw/serial-example/serial.c
@@ -6,6 +6,25 @@
 #include <termios.h>		/* POSIX terminal control definitions */
 
 
+void serial_config(int fd)
+{
+	struct termios options;
+	tcgetattr(fd, &options);
+	cfsetispeed(&options, B115200);
+	cfsetospeed(&options, B115200);
+	options.c_cflag |= (CLOCAL | CREAD);  // Enable local mode
+	options.c_cflag &= ~PARENB;  // Disable parity
+	options.c_cflag &= ~CSTOPB;  // One stop bit
+	options.c_cflag &= ~CSIZE;  // Clear data size bits
+	options.c_cflag |= CS8;	 // Set 8 bits data size
+	options.c_cflag &= ~CRTSCTS;	// Disable hardware flow control
+	options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);  // Set raw input (unbuffered, no echo)
+	options.c_iflag &= ~(IXON | IXOFF | IXANY);  // Disable software flow control
+	options.c_oflag &= ~OPOST;  // Set raw output
+	tcsetattr(fd, TCSAFLUSH, &options);
+}
+
+
 int main(int argc, char * argv[])
 {
 	if (argc < 2) {
@@ -19,7 +38,9 @@ int main(int argc, char * argv[])
 		return 1;
 	}
 
-	fcntl(fd, F_SETFL, 0);
+	fcntl(fd, F_SETFL, FNDELAY);  // Set non-blocking read
+
+	//serial_config(fd);
 
 	int n = write(fd, "S", 1);
 	if (n < 0) {
@@ -34,8 +55,8 @@ int main(int argc, char * argv[])
 			perror("Read failed");
 			return 1;
 		} else if (n == 0) {
-			fprintf(stderr, "EOF\n");
-			break;
+			//fprintf(stderr, "EOF\n");
+			//break;
 		} else {
 			buffer[n] = '\0';
 			printf("%s", buffer);
