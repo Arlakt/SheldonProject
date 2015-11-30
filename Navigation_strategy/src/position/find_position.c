e036bab541928d5cc0a2dd31447936adf96965f0
Elskou <yorrick.barnay@gmail.com>
2015-12-01 00:14:05 +0100

    Cleaning files

--- Navigation_strategy/src/position/find_position.c
+++ Navigation_strategy/src/position/find_position.c
@@ -1,4 +1,4 @@
-#include "./../API/find_position.h"
+#include "./../API/track_position.h"
 
 #define SIZE_ARRAY 8
 
@@ -7,6 +7,14 @@
 //front : 0 ; back : 180 ; right : 90 ; left : 270
 int receiver_position[SIZE_ARRAY] = {0, 45, 90, 135, 180, 225, 270, 315};
 
+//shared variable of position of the beacon
+t_position pos = {10,100};
+
+//mutex arbitrating computing and tracking
+extern pthread_mutex_t compute_pos_mux;
+extern pthread_mutex_t track_pos_mux;
+
+//finds the receiver receiving the maximum signal
 int find_maximum(int * signals_power)
 {
     int max = 0;
@@ -18,11 +26,32 @@ int find_maximum(int * signals_power)
     return max;
 }
 
-int basic_position(int * signals_power, t_position * pos)
+//finds the receiver with the maximum value
+//signals_power is an array containing the signal value on each receiver
+int basic_position(int * signals_power, t_position * pos_aux)
 {
     int angle_max = find_maximum(signals_power);
-    (*pos).angle = receiver_position[angle_max];
-    (*pos).distance = signals_power[angle_max];
+    (*pos_aux).angle = receiver_position[angle_max];
+    (*pos_aux).distance = signals_power[angle_max];
 
     return 0;
+}
+
+//function designed to be the main of a thread
+//put the position of the beacon in shared variable pos
+void * compute_position(void * arg){
+    int i = 0;
+    while(i < 10){
+        //locks its own mutex
+        pthread_mutex_lock(&compute_pos_mux);
+
+	int * signals_power = (int *) arg;
+        basic_position(signals_power, &pos);
+	printf("**************************\nCompute\n**************************\n");
+
+	//release the mutex for printing
+        pthread_mutex_unlock(&track_pos_mux);
+	i++;
+    }
+    pthread_exit(NULL);
 }
---
+++ Navigation_strategy/src/position/track_position.c
@@ -0,0 +1,64 @@
+#include "./../API/track_position.h"
+
+//shared variable of position of the beacon
+extern t_position pos;
+
+//mutex arbitrating computing and tracking
+extern pthread_mutex_t compute_pos_mux;
+extern pthread_mutex_t track_pos_mux;
+
+//used to prototype track_position
+//prints the position of the beacon anytime the thread is active
+void * print_position(void * arg){
+
+    (void) arg;
+    int i = 0;
+    while(i < 10){
+
+        //locks its own mutex
+        pthread_mutex_lock(&track_pos_mux);
+
+	printf("Print Angle : %d \nDistance : %d \n", pos.angle, pos.distance);
+        
+        //release the calculating mutex
+        pthread_mutex_unlock(&compute_pos_mux);
+
+	i++;
+    }
+    pthread_exit(NULL);
+}
+
+//function designed to be the main of a thread
+//send the movement orders of the drone in order to follow the beacon
+void * track_position(void * arg){
+
+    struct timeval old_tv = {0};
+    struct timeval tv = {0};
+    long int elapsed_time = 0; // in microsecondss
+
+    gettimeofday(&old_tv, NULL); 
+
+    (void) arg;
+    int i = 0;
+    while(i < 10){
+        while (elapsed_time < 40000)
+	{
+	    gettimeofday(&tv, NULL);
+	    elapsed_time = (tv.tv_sec-old_tv.tv_sec)*1000000 + (tv.tv_usec-old_tv.tv_usec);
+	}
+
+        //locks its own mutex
+        pthread_mutex_lock(&track_pos_mux);
+	//send the move command
+	printf("Move toward : Angle : %d \nDistance : %d \nElapsed time : %ld\n", pos.angle, pos.distance, elapsed_time);
+       // printf("sec: %ld usec : %ld\n", tv.tv_sec, tv.tv_usec);
+        //release the calculating mutex
+        pthread_mutex_unlock(&compute_pos_mux);
+
+        gettimeofday(&old_tv, NULL);
+	elapsed_time=0;
+
+	i++;
+    }
+    pthread_exit(NULL);
+}
